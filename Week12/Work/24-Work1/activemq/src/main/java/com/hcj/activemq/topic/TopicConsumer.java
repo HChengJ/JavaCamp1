2c57f76831604de5a9eda08158c866273a10dcf4
胡成疆 <kocm520@gmail.com>
2021-09-13 11:44:02 +0800

    24-work1-adjust

--- Week12/Work/24-Work1/activemq/src/main/java/com/hcj/activemq/topic/TopicConsumer.java
+++ Week12/Work/24-Work1/activemq/src/main/java/com/hcj/activemq/topic/TopicConsumer.java
@@ -1,5 +1,6 @@
 package com.hcj.activemq.topic;
 
+import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
@@ -12,19 +13,46 @@ public class TopicConsumer {
     private static final String url="tcp://localhost:61616";
     private  static final String topicName="topic1";
     public static void main(String[] args) throws JMSException {
-        ConnectionFactory connectionFactory=new ActiveMQConnectionFactory(url);
-        Connection connection = connectionFactory.createConnection();
-        connection.start();
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createTopic(topicName);
-        MessageConsumer consumer=session.createConsumer(destination);
-        consumer.setMessageListener(message -> {
-            TextMessage textMessage= (TextMessage) message;
-            try {
-                System.out.println("接收消息:"+textMessage.getText());
-            } catch (JMSException e) {
-                e.printStackTrace();
-            }
-        });
+        // ConnectionFactory ：连接工厂，JMS 用它创建连接
+        ConnectionFactory connectionFactory;
+        // Connection ：JMS 客户端到JMS Provider 的连接
+        Connection connection = null;
+        // Session： 一个发送或接收消息的线程
+        Session session;
+        // Destination ：消息的目的地;消息发送给谁.
+        Destination destination;
+        // 消费者，消息接收者
+        MessageConsumer consumer;
+        connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER,ActiveMQConnection.DEFAULT_PASSWORD, url);
+        try {
+            // 构造从工厂得到连接对象
+            connection = connectionFactory.createConnection();
+            //当生产者采用持久化策略时，ClientID用以别区哪个客户端已经接收过
+            //connection.setClientID("hh");
+            // 启动
+            connection.start();
+            // 获取操作连接
+            session = connection.createSession(Boolean.FALSE,Session.AUTO_ACKNOWLEDGE);
+            // 获取session注意参数值mytopic是一个服务器的topic，须在在ActiveMq的console配置
+            destination = session.createTopic(topicName);
+            consumer = session.createConsumer(destination);
+            System.out.println("我是订阅者1");
+            consumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    if(message instanceof TextMessage){
+                        try {
+                            TextMessage text=(TextMessage)message;
+                            System.out.println("收到消息" + text.getText());
+                        } catch (JMSException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- Week12/Work/24-Work1/activemq/src/main/java/com/hcj/activemq/topic/TopicProducer.java
+++ Week12/Work/24-Work1/activemq/src/main/java/com/hcj/activemq/topic/TopicProducer.java
@@ -1,5 +1,6 @@
 package com.hcj.activemq.topic;
 
+import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
@@ -12,17 +13,51 @@ public class TopicProducer {
     private static final String url = "tcp://localhost:61616";
     private static final String topicName = "topic1";
     public static void main(String[] args) throws JMSException {
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
-        Connection connection = connectionFactory.createConnection();
-        connection.start();
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createTopic(topicName);
-        MessageProducer producer = session.createProducer(destination);
-        for (int i = 0; i < 100; i++) {
-            TextMessage textMessage = session.createTextMessage("topic message" + i);
-            producer.send(textMessage);
-            System.out.println("发送消息：" + textMessage.getText());
+        System.out.println("TopicSender启动了...");
+        // ConnectionFactory ：连接工厂，JMS 用它创建连接
+        ConnectionFactory connectionFactory;
+        // Connection ：JMS 客户端到JMS Provider 的连接
+        Connection connection = null;
+        // Session： 一个发送或接收消息的线程
+        Session session;
+        // Destination ：消息的目的地;消息发送给谁
+        //Destination destinaction;
+        //topic 话题
+        Topic topic;
+        // MessageProducer：消息发送者
+        MessageProducer producer;
+        // TextMessage message;
+        // 构造ConnectionFactory实例对象，此处采用ActiveMq的实现jar
+        connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER,ActiveMQConnection.DEFAULT_PASSWORD, url);
+        try {
+            // 构造从工厂得到连接对象
+            connection = connectionFactory.createConnection();
+            // 启动
+            connection.start();
+            // 获取操作连接
+            session = connection.createSession(Boolean.TRUE,Session.AUTO_ACKNOWLEDGE);
+            // 获取session注意参数值mytopic是一个服务器的topic，须在在ActiveMq的console配置
+            topic = session.createTopic(topicName);
+            // 得到消息生成者【发送者】
+            producer = session.createProducer(topic);
+            // 设置不持久化
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+            // 消息体
+            for (int i = 1; i <= 100; i++) {
+                TextMessage message = session.createTextMessage("topic message" + i);
+                System.out.println("Sender发送消息：" + "topic message" + i);
+                producer.send(message);
+            }
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != connection)
+                    connection.close();
+            } catch (Throwable ignore) {
+            }
         }
-        connection.close();
+
     }
 }
